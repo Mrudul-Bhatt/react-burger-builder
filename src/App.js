7e1c0807f99eb65481b9985928dd22a34b1a261c
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-05-12 22:58:13 +0530

    added proptypes validation in brgeringredient component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
 import React, { Component } from "react";
 import classes from "./App.module.css";
-import Person from "../components/Persons/Person/Person";
-//import Radium from "radium";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
-  state = {
-    person: [
-      { name: "m1", age: 1 },
-      { name: "m2", age: 2 },
-      { name: "m3", age: 3 },
-    ],
-    show: false,
-    changeNameVal: false,
-  };
-
-  changeShow = () => {
-    this.setState({
-      show: !this.state.show,
-    });
-  };
-  showNameChange = () => {
-    this.setState({
-      changeNameVal: !this.state.changeNameVal,
-    });
-  };
-
-  changeName = (e) => {
-    this.setState({
-      person: [
-        { name: e.target.value, age: 1 },
-        { name: "m2", age: 2 },
-        { name: "m3", age: 3 },
-      ],
-    });
-  };
-
   render() {
     return (
-      <div className={classes.App}>
-        <h1>Hello</h1>
-        <button onClick={this.changeShow}>Show Values</button>
-        {this.state.show ? (
-          <div>
-            <Person
-              changeNameVal={this.showNameChange}
-              changeName={this.changeName}
-              changeNameValState={this.state.changeNameVal}
-              name={this.state.person[0].name}
-            >
-              children1
-            </Person>
-          </div>
-        ) : null}
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }

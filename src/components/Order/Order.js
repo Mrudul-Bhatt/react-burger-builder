b2ebc864042f9799233f96e6a7a35f75b97465b3
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-05-27 01:30:34 +0530

    authentication finished

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,43 +1,44 @@
-import React from "react";
+import React from 'react';
 
-import classes from "./Order.module.css";
+import classes from './Order.module.css';
 
 const order = (props) => {
-  const ingredients = [];
+    const ingredients = [];
 
-  for (let ingredientName in props.ingredients) {
-    ingredients.push({
-      name: ingredientName,
-      amount: props.ingredients[ingredientName],
+    for (let ingredientName in props.ingredients) {
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName],
+        });
+    }
+    //we made this => ingredients = [{name: slad, amount: 1}, {name: cheese, amount: 2},{....}, {....}]
+
+    const ingredientOutput = ingredients.map((ig) => {
+        return (
+            <span
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px',
+                }}
+                key={ig.name}
+            >
+                {ig.name} ({ig.amount})
+            </span>
+        );
     });
-  }
-  //we made this => ingredients = [{name: slad, amount: 1}, {name: cheese, amount: 2},{....}, {....}]
 
-  const ingredientOutput = ingredients.map((ig) => {
     return (
-      <span
-        style={{
-          textTransform: "capitalize",
-          display: "inline-block",
-          margin: "0 8px",
-          border: "1px solid #ccc",
-          padding: "5px",
-        }}
-        key={ig.name}
-      >
-        {ig.name} ({ig.amount})
-      </span>
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>
+                Price:
+                <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+            </p>
+        </div>
     );
-  });
-
-  return (
-    <div className={classes.Order}>
-      <p>Ingredients: {ingredientOutput}</p>
-      <p>
-        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
-      </p>
-    </div>
-  );
 };
 
 export default order;
